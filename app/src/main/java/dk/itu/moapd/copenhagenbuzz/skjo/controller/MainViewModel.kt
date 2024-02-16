12d399922b6a22c48ee8c6fb94742181dad0f853
JonasSkjodt1 <skjo@itu.dk>
2024-02-16 12:37:25 +0100

    comment clarifications

--- app/src/main/java/dk/itu/moapd/copenhagenbuzz/skjo/controller/MainViewModel.kt
+++ app/src/main/java/dk/itu/moapd/copenhagenbuzz/skjo/controller/MainViewModel.kt
@@ -3,8 +3,10 @@ package dk.itu.moapd.copenhagenbuzz.skjo.controller
 import androidx.lifecycle.ViewModel
 
 /**
- * ViewModel keeps an activity’s user interface state data in
- * memory across configuration changes.
+ * MainViewModel
+ * MainViewModel extends ViewModel which is an android jetpack feature.
+ * It keeps an activity’s user interface state data in memory across configuration changes.
+ * This means that data will still be stored when the phone is rotated.
  */
 class MainViewModel : ViewModel() {
     var isLoggedIn: Boolean = false
